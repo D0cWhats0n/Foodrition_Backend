50ef61cd7c3951b1239aa3cf795600239da5bec1
Märkle Johannes <Johannes.Maerkle@factore.tcon-group.de>
2019-02-11 14:51:00 +0100

    Added tests and debugged ModelFactory

--- foodrition_backend/foodrition_api/services/ml_model.py
+++ foodrition_backend/foodrition_api/services/ml_model.py
@@ -1,40 +1,51 @@
 from django.core.cache import cache
-import keras.model import model_from_json
+from keras.models import model_from_json
 from keras.applications.resnet50 import preprocess_input
 import json
 import cv2
 from os.path import join, dirname
+import tensorflow as tf
+import numpy as np
 
 
 class ModelFactory(object):
     model_cache_key = 'model_cache'
     map_cache_key = 'map_cache'
-    model_folder = join(dirname(__file__), './model')  # get current directory'
+    model_folder = join(dirname(__file__), 'model')  # get current directory'
     img_res = (224, 224)
+    graph = None
 
     @staticmethod
     def predict(img):
+        img_res = ModelFactory.img_res
+        if ModelFactory.graph is None:
+            # Load and cache model if not done yet, and set ModelFactory.graph
+            # (see issue https://github.com/keras-team/keras/issues/2397)
+            ModelFactory.model_map, ModelFactory.model = ModelFactory.load_model_in_cache()
 
-        model = cache.get(ModelFactory.model_cache_key)     # get model from cache
-        model_map = cache.get(ModelFactory.map_cache_key)
+        with ModelFactory.graph.as_default():
+            model = cache.get(ModelFactory.model_cache_key)     # get model from cache
+            model_map = cache.get(ModelFactory.map_cache_key)
 
-        # Load and cache model if not done yet
-        if model is None:
-            ModelFactory.model_map, ModelFactory.model = _load_model_in_cache()
+            img = cv2.imdecode(np.fromstring(img.read(), np.uint8), cv2.IMREAD_UNCHANGED)
+            img = cv2.resize(img, ModelFactory.img_res)
+            img = preprocess_input(img)
 
-        img = cv2.resize(img, ModelFactory.img_res)
-        img = preprocess_input(img)
-
-        return model_map[model.predict(img)]
+            predict_vec = model.predict(img.reshape(-1, img_res[0], img_res[1], 3))[0]
+            predict_id = np.argmax(predict_vec)
+            # TODO map to food table for nutrition info
+            print(f"Predicted class {predict_id} with precentage {predict_vec[predict_id]*100}%")
+            return predict_id
 
 
     @staticmethod    
-    def _load_model_in_cache():
+    def load_model_in_cache():
         model_folder = ModelFactory.model_folder
         model_cache_key = ModelFactory.model_cache_key
         map_cache_key = ModelFactory.map_cache_key
 
         try:
+            print("Trying to load model in cache")
             model_json_file = open(join(model_folder, "model.json"), 'r')
             model_json = model_json_file.read()
             model_json_file.close()
@@ -43,17 +54,17 @@ class ModelFactory(object):
             
             # load weights for model
             model.load_weights(join(model_folder, "model.h5"))
-
+            ModelFactory.graph = tf.get_default_graph()
             # save in the cache
-            cache.set(model_cache_key, model, None) 
+            cache.set(model_cache_key, model, None)
 
             with open(join(model_folder, 'map.json')) as map_file:
                 model_map = json.load(map_file)
-                cache.set(map_cache_key)
+                cache.set(map_cache_key, model_map)
             
             return (model_map, model)
 
-        except ValueError:
+        except TypeError:
             print("Can't load model from file! ")
             raise
 
--- foodrition_backend/foodrition_api/tests.py
+++ foodrition_backend/foodrition_api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .services.ml_model import ModelFactory
+from django.core.cache import cache
 
-# Create your tests here.
+
+class MLModelTest(TestCase):
+
+    def test_loading_model_to_cache(self):
+        model_map, model = ModelFactory.load_model_in_cache()
+        assert(hasattr(model, 'predict'))
+        assert(isinstance(model_map, dict))
+
+        # Make also sure that it lands in cache
+        model = cache.get(ModelFactory.model_cache_key)     
+        model_map = cache.get(ModelFactory.map_cache_key)
+        assert(hasattr(model, 'predict'))
+        assert(isinstance(model_map, dict))
